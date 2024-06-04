24011553322c760c959f19df17882c5787c56a80
sandeep kumar <sandeepypb@gmail.com>
2024-06-04 09:52:52 +0530

    ✨ Feat: create components

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello from ReactJS"),
-    React.createElement("h2", {}, "Hello from ReactJS"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello from ReactJS"),
-    React.createElement("h2", {}, "Hello from ReactJS"),
-  ]),
-]);
+const heading = <h1>Hello</h1>;
+
+// React components
+// Class based components - OLD
+// Functional components - NEW
+
+const Parent = () => {
+  return (
+    <div>
+      <div>Parent</div>
+      <Child />
+      {Child()}
+      <Child></Child>
+      <App />
+      {heading}
+      {100 + 200}
+      {console.log("hello")}
+    </div>
+  );
+};
+
+const Child = () => <div>Child</div>;
+
+function App() {
+  return <div>Normal function</div>;
+}
+
+console.log(App);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Parent />);
